e6c4ecc60554251fbfbfc86e2dde25258062d056
swoo <kokodd1234@gmail.com>
2026-04-06 14:05:39 +0900

    v7

--- src/main/java/org/example/cicd/CicdController.java
+++ src/main/java/org/example/cicd/CicdController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CicdController {
     @GetMapping("/cicd")
     public String test() {
-        return "v6";
+        return "v7";
     }
 }
