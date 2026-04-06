c4fd9e39c4ab854ca7e0095b12f282cc7126faa6
swoo <kokodd1234@gmail.com>
2026-04-06 10:15:48 +0900

    v1

---
+++ src/main/java/org/example/cicd/CicdController.java
@@ -0,0 +1,15 @@
+package org.example.cicd;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/cicd")
+@RestController
+public class CicdController {
+    @GetMapping("/cicd")
+    public String test() {
+        return "v1";
+    }
+}
